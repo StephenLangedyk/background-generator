f195f0a36ccfff81f14d800d000c8dec421e7c9b
StephenLangedyk <39946821+StephenLangedyk@users.noreply.github.com>
2018-07-05 13:00:30 +0200

    New files

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("bodyColor");
 
-const colorBox = document.querySelector("input");
 
 
 
-function getColorValue () {
-	css.textContent = colorBox.value;
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+	css.textContent = body.style.background + ";";
 
 }
 
-getColorValue();
+
+function randomNumber() {
+	var x = Math.floor((Math.random() * 255) + 1);
+	var z = Math.floor((Math.random() * 255) + 1);
+	var y = Math.floor((Math.random() * 255) + 1);
+
+    
+    body.style.background = 
+	"linear-gradient(to right, rgb(" + x + "," + y + "," + z 
+	+ "), rgb(" + y + "," + z + "," + x + "))"; 
+	css.textContent = body.style.background + ";";
+}
+
+
+
+color1.addEventListener("input", setGradient);
+	
+
+color2.addEventListener("input", setGradient);
+
+
+
